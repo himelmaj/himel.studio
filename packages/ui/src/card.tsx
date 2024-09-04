4173fa2feca9b6ff9f767def6cd6c9df97543af1
Himel Majumder <himelmajumder1@gmail.com>
2024-09-04 19:05:36 +0200

    feat: Add support for tailwind CSS

--- packages/ui/src/card.tsx
+++ packages/ui/src/card.tsx
@@ -28,3 +28,13 @@ export function Card({
     </a>
   );
 }
+
+
+export function Gradient({ conic, className, small, }: { small?: boolean; conic?: boolean; className?: string }): JSX.Element {
+  return (
+    <span
+      className={`absolute mix-blend-normal will-change-[filter] rounded-[100%] ${small ? "blur-[32px]" : "blur-[75px]"
+        } ${conic ? "bg-glow-conic" : ""} ${className}`}
+    />
+  )
+}
